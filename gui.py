1ea2f834b74353ae7a054a34d4478de97b81bd78
Hadir Khan <hadirkhan10@gmail.com>
2021-07-31 23:36:51 +0100

    created the option to init design for the first time

--- gui.py
+++ gui.py
@@ -10,6 +10,12 @@ from docker.api import volume
 pdk_root = ""
 openlane_root = ""
 
+def start():
+    project_name_entry.grid_forget()
+    create_project_btn.grid_forget()
+    label.configure(text="Creating your project please wait..")
+    root.after(1, create_design)
+
 def read_env_variables():
     try:
         global pdk_root
@@ -26,11 +32,12 @@ def read_env_variables():
 def open_existing_project():
     print("opening existing project...")
 
-def create_design(entryField):
-    print("entry is " + entryField.get())
+def create_design():
+    print("entry is " + project_name.get())
     print(openlane_root)
+    
     client = docker.from_env()
-    container = client.containers.run('efabless/openlane:current', command='./flow.tcl -design ' + entryField.get() + ' -init_design_config', 
+    container = client.containers.run('efabless/openlane:current', command='./flow.tcl -design ' + project_name.get() + ' -init_design_config', 
         volumes={openlane_root: {'bind': '/openLANE_flow', 'mode': 'rw'},
                  pdk_root: {'bind': pdk_root, 'mode':'rw'}},
         environment=["PDK_ROOT=" + pdk_root],
@@ -38,24 +45,34 @@ def create_design(entryField):
         detach=True,
         stdout=True,
         stderr=True)
+    label.configure(text="Done creating your project. Check the output below if it is a success or error")
+    prep_design_console.insert('1.0', container.logs().decode('utf-8'))
+    prep_design_console.grid(row=2, column=0)
+    proceed_btn.grid(row=3, column=0, columnspan=3, pady=10)
 
-    text = Text(frame, width=80, height=20)
-    text.insert('1.0', container.logs().decode('utf-8'))
-    text.grid(row=3, column=0)
 def create_new_project():
-
     print("creating new project...")
     if read_env_variables() == True:
         label.configure(text="Set a name for the project")
         open_existing_btn.grid_forget()
         open_new_btn.grid_forget()
-        project_name = StringVar()
-        project_name_entry = ttk.Entry(frame, textvariable=project_name)
         project_name_entry.grid(row=2, column=0)
-        create_project_btn = ttk.Button(frame, text="Create", command= lambda: create_design(project_name))
         create_project_btn.grid(row=2, column=1)
     else:
         pass
+
+def add_verilog_file():
+    pass
+
+def proceed():
+    prep_design_console.grid_forget()
+    proceed_btn.grid_forget()
+    label.configure(text="Upload your verilog files")
+    add_verilog_btn.grid(row=2, columnspan=3)
+
+def upload_verilog_files():
+
+    pass
 
 root = Tk()
 root.title("OpenLANE Graphical User Interface")
@@ -67,4 +84,10 @@ frame.grid(row=0, column=0, padx=40, pady=40)
 label.grid(row=1, column=0, columnspan=3)
 open_existing_btn.grid(row=2, column=0, columnspan=1)
 open_new_btn.grid(row=2, column=1, columnspan=1)
+project_name = StringVar()
+project_name_entry = ttk.Entry(frame, textvariable=project_name)
+create_project_btn = ttk.Button(frame, text="Create", command=start)
+prep_design_console = Text(frame, width=80, height=20)
+proceed_btn = ttk.Button(frame, text="Proceed", command=proceed)
+add_verilog_btn = ttk.Button(frame, text="Add Verilog", command=upload_verilog_files)
 root.mainloop()
